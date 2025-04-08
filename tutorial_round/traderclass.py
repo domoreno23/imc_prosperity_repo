8da5047b13a4168d7a82856328a10fd5201da695
Damian <dmoreno19@outlook.com>
2025-04-08 12:34:07 -0500

    Co-authored-by: CarlosAtienza <CarlosAtienza@users.noreply.github.com> Co-authored-by: DHypes <DHypes@users.noreply.github.com> Co-authored-by: Sai Patel <SaiPatel2006@users.noreply.github.com>

--- tutorial_round/traderclass.py
+++ tutorial_round/traderclass.py
@@ -3,51 +3,106 @@ from typing import Dict, List
 import string
 import json
 
-class Trader:
 
-    position_limit = {
-        "RAINFOREST_RESIN" : 50,
-        "KELP" : 50
-    }
+class Strategy:
+  """
+  Abstract Strategy class
+  """
+
+  def __init__(self, symbol: str, limit: int):
+    self.symbol = symbol
+    self.limit = limit
+    self.position = 0
+
+
+  def run(self, state: TradingState) -> list[Order]:
+      
+
+      self.orders = []
+      for product in state.order_depths: # bids and asks that we can take. Someone is offering to sell or buy
+          order_depth: OrderDepth = state.order_depths[product]
+          orders: List[Order] = []
+          
+          match product:
+            case 1: 
+              self.orders = RainForestResin.run(order_depth, orders, product)
+            case 2:
+              self.orders = Kelp.run()
+              
+      return self.orders
+
+
+class Kelp(Strategy):
+  pass
+
+
+class RainForestResin(Strategy):
+  def __init__(self, symbol: str, limit: int):
+    super().__init__(symbol=symbol, limit=limit)
+  
+  def run(self, state: TradingState) -> List[Order]:
+    orders=[]
+    acceptable_price = 10000
+    if self.symbol not in state.order_depths:
+      return orders
+    else:
+      order_depth = state.order_depths[self.symbol]
     
-    def run(self, state: TradingState):
-        print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
-
-				# Orders to be placed on exchange matching engine
-        result = {}
-        for product in state.order_depths: # bids and asks that we can take. Someone is offering to sell or buy
-            order_depth: OrderDepth = state.order_depths[product]
-            orders: List[Order] = []
-
-            # TODO: calculate this value maybe make a method for this les go
-            acceptable_price = 10000# Participant should calculate this value
-            print("Acceptable price : " + str(acceptable_price)) # a fair price, what it should revert to
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-    
-            if len(order_depth.sell_orders) != 0:
+      if len(order_depth.sell_orders) != 0:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 if int(best_ask) < acceptable_price:
                     print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
+                    orders.append(Order(symbol=self.symbol, best_ask=best_ask, best_ask_amount=-best_ask_amount))
     
-            if len(order_depth.buy_orders) != 0:
+      if len(order_depth.buy_orders) != 0:
                 best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
                 if int(best_bid) > acceptable_price:
                     print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
+                    orders.append(Order(symbol=self.symbol, best_bid=best_bid, best_bid_amount=-best_bid_amount))
+      return orders
+
+class Squid_Ink(Strategy):
+  pass
+
+
+class Trader:
+    def __init__(self):
+    
+      # Create each strategy for each product here
+      self.strategies = {
+        "RAINFOREST_RESIN" : RainForestResin("RAINFOREST_RESIN", 50),
+        "KELP": Kelp("KELP", 50)
+      }
+
+      self.trader_data = {}  #storing state between rounds
+
+    def run(self, state: TradingState):
+        print("traderData: " + state.traderData)
+        print("Observations: " + str(state.observations))
+        result = {}
+				
+        if state.traderData:
+            try:
+              self.trader_data = json.loads(state.traderData)
+            except:
+              self.trader_data = {}
+
+        for product, strategy in self.strategies.items():
+            if product in state.order_depths:
+              try:
+                  orders = strategy.run(state=state)
+                  result[product] = orders
+              except:
+                print(f"Error executing strategy for {product}")
+            
+                result[product]
             
             result[product] = orders
     
 		    # String value holding Trader state data required. 
 				# It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE" 
+        trader_data_str = json.dumps(self.trader_data)
         
 				# Sample conversion request. Check more details below. 
         conversions = 1
-        return result, conversions, traderData
-      
-      
-      ## Imm a delete this part
-
-      
+        return result, conversions, trader_data_str
