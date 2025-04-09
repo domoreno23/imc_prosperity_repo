2991aa81dc3aecc277873111593804ebb7767422
CarlosAtienza <atienzacarlos72@gmail.com>
2025-04-09 17:36:39 -0500

    Updated traderclass

--- tutorial_round/traderclass.py
+++ tutorial_round/traderclass.py
@@ -4,79 +4,93 @@ import string
 import json
 
 
-class Strategy:
-  """
-  Abstract Strategy class
-  """
+class Kelp():
+  def __init__(self, symbol, limit):
+    self.symbol = symbol
+    self.limit = limit
 
+  def calculate_value(self, order_depth: OrderDepth, depth: int):
+     if len(order_depth.sell_orders) != 0 and len(order_depth.buy_orders) != 0:
+      buy_orders = order_depth.buy_orders
+      sell_orders = order_depth.sell_orders
+      
+      #Sort the top n volumes in the each side
+      top_buys = sorted(buy_orders.items(), key=lambda x: -x[0])[:depth]
+      top_sells = sorted(sell_orders.items(), key=lambda x: x[0])[:depth]
+
+      total_volume = 0 
+      weighted_sum = 0
+
+      #Calculate the weighted average
+      for price, volume in top_buys + top_sells:
+          total_volume += abs(volume)
+          weighted_sum += price * abs(volume)
+
+      return weighted_sum / total_volume if total_volume != 0 else None
+     
+  def run(self, state: TradingState):
+    orders=[]
+    
+    order_depth = state.order_depths[self.symbol]
+    acceptable_price = self.calculate_value(order_depth, 5)
+           
+    if len(order_depth.sell_orders) != 0:
+      best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+      if int(best_ask) < acceptable_price:
+          orders.append(Order(self.symbol, best_ask, -best_ask_amount))
+    
+    if len(order_depth.buy_orders) != 0:
+      best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+      if int(best_bid) > acceptable_price:       
+          orders.append(Order(self.symbol, best_bid, -best_bid_amount))
+   
+     
+    return orders
+        
+class RainForestResin():
   def __init__(self, symbol: str, limit: int):
     self.symbol = symbol
     self.limit = limit
-    self.position = 0
-
-
-  def run(self, state: TradingState) -> list[Order]:
-      
-
-      self.orders = []
-      for product in state.order_depths: # bids and asks that we can take. Someone is offering to sell or buy
-          order_depth: OrderDepth = state.order_depths[product]
-          orders: List[Order] = []
-          
-          match product:
-            case 1: 
-              self.orders = RainForestResin.run(order_depth, orders, product)
-            case 2:
-              self.orders = Kelp.run()
-              
-      return self.orders
-
-
-class Kelp(Strategy):
-  def __init__(self, symbol: str, limit: int):
-    super().__init__(symbol=symbol, limit=limit)
-    
-  def run(self, state: TradingState) -> List[Order]:
-    pass
-  pass
-
-
-class RainForestResin(Strategy):
-  def __init__(self, symbol: str, limit: int):
-    super().__init__(symbol=symbol, limit=limit)
   
   def run(self, state: TradingState) -> List[Order]:
     orders=[]
     acceptable_price = 10000
-    if self.symbol not in state.order_depths:
-      return orders
-    else:
-      order_depth = state.order_depths[self.symbol]
+    order_depth = state.order_depths[self.symbol]
+           
+    if len(order_depth.sell_orders) != 0:
+      best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+      if int(best_ask) < acceptable_price:
+          orders.append(Order(self.symbol, best_ask, -best_ask_amount))
     
-    #Max out the buys
-    #If price is less than 10000 then buy
-      if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(symbol=self.symbol, best_ask=best_ask, best_ask_amount=-best_ask_amount))
-    
-    #If price is more than 10000 sell it
-      if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(symbol=self.symbol, best_bid=best_bid, best_bid_amount=-best_bid_amount))
-      return orders
+    if len(order_depth.buy_orders) != 0:
+      best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+      if int(best_bid) > acceptable_price:       
+          orders.append(Order(self.symbol, best_bid, -best_bid_amount))
+   
+    return orders
 
-
-class Squid_Ink(Strategy):
+class SquidInk():
   def __init__(self, symbol: str, limit: int):
-    super().__init__(symbol=symbol, limit=limit)
-    
+    self.symbol = symbol
+    self.limit = limit
+
   def run(self, state: TradingState) -> List[Order]:
-    pass
-  pass
+    order_depth: OrderDepth = state.order_depths[self.symbol]
+    orders: List[Order] = []
+    acceptable_price = 2000  # Participant should calculate this value
+           
+    if len(order_depth.sell_orders) != 0:
+      best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+      if int(best_ask) < acceptable_price:
+          orders.append(Order(self.symbol, best_ask, -best_ask_amount))
+    
+    if len(order_depth.buy_orders) != 0:
+      best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+      if int(best_bid) > acceptable_price:       
+          orders.append(Order(self.symbol, best_bid, -best_bid_amount))
+
+    return orders
+  
 
 
 class Trader:
@@ -86,7 +100,7 @@ class Trader:
       self.strategies = {
         "RAINFOREST_RESIN" : RainForestResin("RAINFOREST_RESIN", 50),
         "KELP": Kelp("KELP", 50),
-        "SQUID_INK": Squid_Ink("SQUID_INK", 0) #Put number here
+        "SQUID_INK": SquidInk("SQUID_INK", 50)
       }
 
       self.trader_data = {}  #storing state between rounds
@@ -95,27 +109,20 @@ class Trader:
         print("traderData: " + state.traderData)
         print("Observations: " + str(state.observations))
         result = {}
-				
-        if state.traderData:
-            try:
-              self.trader_data = json.loads(state.traderData)
-            except:
-              self.trader_data = {}
 
+        #For each product, check if product is in orderDepth. If so run strategy
         for product, strategy in self.strategies.items():
             if product in state.order_depths:
               try:
                   orders = strategy.run(state=state)
                   result[product] = orders
-              except:
-                print(f"Error executing strategy for {product}")
+              except Exception as e:
+                print(f"Error executing strategy for {product}: {str(e)}")
             
                 result[product]
             
             result[product] = orders
-    
-		    # String value holding Trader state data required. 
-				# It will be delivered as TradingState.traderData on next execution.
+  
         trader_data_str = json.dumps(self.trader_data)
         
 				# Sample conversion request. Check more details below. 
